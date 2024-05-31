366882f0591acca867f00fb4a135c9d6a5984c3d
Milind241 <milinddadore123@gmail.com>
2024-05-31 17:53:51 +0530

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+const app = express();
+const port=process.env.PORT;
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
+app.use(express.json());
+app.use(cookieParser()); // Call the function
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+
+
+app.listen(port, () => {
+    console.log("Server is running!",port);
+});
